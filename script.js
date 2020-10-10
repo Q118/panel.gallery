46d3421628fec45fd6f5be7ddaed927aaa4418a4
Q118 <shelbyfish91@gmail.com>
2020-10-10 14:32:21 -0700

    functionality for transition

---
+++ script.js
@@ -0,0 +1,26 @@
+// TODO: 
+//function to apply the classes to the panels
+//event listeners on the click to ignite function
+
+//variables for panels
+const panel1 = document.querySelector(".panel1");
+const panel2 = document.querySelector(".panel2")
+const panel3 = document.querySelector(".panel3")
+
+function openUp() {
+    this.classList.add("open-active");
+    this.classList.add("open");
+    console.log("yyo")
+}
+
+function closeBack() {
+    this.classList.remove("open-active");
+    this.classList.remove("open");
+}
+
+
+//event listeners
+panel1.addEventListener("click", openUp);
+
+
+panel1.addEventListener("mouseleave", closeBack);
